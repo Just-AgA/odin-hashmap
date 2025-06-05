fd9dd88023a89a9b6d9b97fc15906b65621cce74
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-06-05 20:39:41 +0200

    Add hash function that will be used to hash the keys

---
+++ hashMap.js
@@ -0,0 +1,15 @@
+function HashMap(loadFactor = 0.75, capacity = 16) {
+  let resultArray = new Array(capacity);
+  let count = 0;
+
+  const hash = (key) => {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+    }
+
+    return hashCode;
+  };
+}
