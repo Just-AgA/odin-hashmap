a952ef355928448500c0b77c8831fe957f29643f
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2025-06-10 20:26:13 +0200

    Return all the neccessary methods so they can be used by the instances created

--- hashMap.js
+++ hashMap.js
@@ -182,6 +182,18 @@ function HashMap(loadFactor = 0.75, capacity = 16) {
 
     return entriesArray;
   };
+
+  return {
+    set,
+    get,
+    has,
+    remove,
+    length,
+    clear,
+    keys,
+    values,
+    entries,
+  };
 }
 
 export { HashMap };
